76431a2557e3f3fdf1fb34fd81a3ddbbe9b8c302
irahul <rindhane@users.noreply.github.com>
2020-12-27 13:12:28 +0530

    factor plot_functions.py

--- plot_functions.py
+++ plot_functions.py
@@ -4,17 +4,20 @@ def get_conversion(l):
       for i in l:
         yield i*100/1000
 
-def set_graph_details(axes,**inputs):
+def set_axes_details(axes,**inputs):
     x_label=inputs.get('x_label',None)
-    y_lable=inputs.get('y_label',None)
+    y_label=inputs.get('y_label',None)
+    color=inputs.get('color', 'tab:blue')
     fontsize=inputs.get('fontsize',5)
-    labelsize=inputs.get('fontsize',5)
+    labelsize=inputs.get('labelsize',5)
     axes.set_xlabel(x_label,fontsize=fontsize)
     axes.set_ylabel(y_label,fontsize=fontsize)
-    axes.tick_params(axis="x", labelsize=labelsize)
-    axes.tick_params(axis='y', labelsize=labelsize)
-
-
+    axes.tick_params(axis="x", labelsize=labelsize ,color = color)
+    axes.tick_params(axis='y', labelsize=labelsize, color= color)
+    axes.legend(prop={'size':inputs.get('legedsize',5)})
+    ax.set_title(inputs.get('title',None),fontsize= fontsize+20)
+    if inputs.get('grid', None):
+        ax.grid()
 
 def plot_graph(X, label1,
                A, label2,
@@ -32,7 +35,7 @@ def plot_graph(X, label1,
     plot2=getattr(ax2,kind2, getattr(ax2,'plot'))
     #plotting the data on the figure
     plot(X[0],X[1],label=label1,color='green')
-    plot2(A[0],A[1],label = label2, color='red')
+    plot2(A[0],A[1],label=label2, color='red')
     #configuring the x_axes properties
     ax.set_xlabel(x_label,fontsize = 30) #xlabel
     ax.tick_params(axis="x", labelsize=20)
@@ -48,6 +51,7 @@ def plot_graph(X, label1,
     #configure axes & figure properties
       #ax.set(xlim=[-10000, 140000], xlabel=x_label, ylabel=y_label,title=title)
     ax.legend(prop={'size':30})
+    ax2.legend(prop={'size':30})
     ax.grid()
     #plt.style.use('seaborn-pastel')
     plt.style.use('dark_background')
@@ -57,11 +61,17 @@ def plot_graph(X, label1,
       fig.savefig(file_)
     plt.show()
 
+def figure_details(width,height):
+    return {'figsize': (width,height)}
+
 def plot_distributions(series,
-                        kind = 'hist',
-                        nbins=10):
+                        **inputs):
     #setting upthe figures and axes
-    fig, ax = plt.subplots(figsize=(8, 10))
-    plot=getattr(ax,kind, getattr(ax,'plot'))
-    plot(series, bins=nbins)
+    fig, ax = plt.subplots(**figure_details(
+        inputs.get('figWidth',10),inputs.get('figHeight',10))
+    ax.set_xlim([40,600])
+    set_axes_details(ax,**inputs)
+    plot=getattr(ax,
+                inputs.get('kind','plot'),)
+    plot(series, bins=inputs.get('nbins',20))
     plt.show()
